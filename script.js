851d6f20bb105c39c47cbb60440754400bcfcba5
crealu <cameron.miller294@gmail.com>
2025-02-28 16:57:28 -0800

    Update script.js
    - Add two more animations

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const menu = document.getElementById('menu');
 const swan = document.getElementById('swan');
 const text = document.getElementById("text");
-const squawk = document.getElementById("swan-audio");
-const audio = new Audio("media/squawk.mp3");
+const sound = document.getElementById("swan-audio");
+// const audio = new Audio("media/squawk.mp3");
 
 const burst = new mojs.Burst({
     radius:   { 0: 100 },
     count:    10,
     children: {
-    shape:      'polygon',
-    fill:       { 'cyan' : 'yellow' },
-    radius:     20,
-    rotate:      { 360: 0 },
-    duration:   1000
+        shape:      'polygon',
+        fill:       { 'cyan' : 'yellow' },
+        radius:     20,
+        rotate:      { 360: 0 },
+        duration:   1000,
+        delay:      'stagger(0, 10)'
     }
 });
 
@@ -21,8 +22,28 @@ const burst2 = new mojs.Burst({
     count:    10,
     children: {
         shape:      'polygon',
+        points:     5,
+        fill:       { 'cyan' : 'magenta' },
+        rotate:      { 360: 0 },
+        duration:   1250,
+        delay:      'stagger(10, 20)'
     }
 });
+
+const burst3 = new mojs.Burst({
+    radius:   { 0: 100 },
+    count:    5,
+    degree:   360,
+    children: {
+        fill:       { 'magenta' : 'yellow' },
+        duration:   1500,
+        delay:      'stagger(20, 30)'
+    }
+  });
+
+function squawk() {
+    sound.play();
+}
 
 function cueSwans() {
     swan.style.display = 'block';
@@ -31,13 +52,14 @@ function cueSwans() {
     function frame1() {
         t--;
         console.log(t);
-        if (t % 2 == 0) {
-            swan.src = './media/squawk.svg';
-        } else {
-            swan.src = `./media/swan.svg`
-        }
         if (t == 2) {
             clearInterval(id);
+        }
+        
+        if (t % 2 == 0) {
+            swan.src = 'media/squawk.svg';
+        } else {
+            swan.src = 'media/swan.svg';
         }
     }
 }
@@ -60,11 +82,14 @@ menu.addEventListener("click", (event) => {
         event.target.style.display = "none";
         burst.generate();
         burst.replay();
+        burst2.generate();
+        burst2.replay();
+        burst3.generate();
+        burst3.replay();
         text.classList.add("visible");
         setTimeout(() => {
             text.classList.remove("visible");
-            // squawk.play();
-            audio.play();
+            squawk();
             cueSwans()
         }, 2000);
     }, 2000);
