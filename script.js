c89595885e796e3bec06602b0e21316a6060087f
crealu <cameron.miller294@gmail.com>
2025-02-28 16:23:14 -0800

    Update index.html
    - Use local audio file

--- script.js
+++ script.js
@@ -4,57 +4,65 @@ const text = document.getElementById("text");
 const squawk = document.getElementById("swan-audio");
 
 const burst = new mojs.Burst({
-        radius:   { 0: 100 },
-        count:    10,
-        children: {
+    radius:   { 0: 100 },
+    count:    10,
+    children: {
+    shape:      'polygon',
+    fill:       { 'cyan' : 'yellow' },
+    radius:     20,
+    rotate:      { 360: 0 },
+    duration:   1000
+    }
+});
+
+const burst2 = new mojs.Burst({
+    radius:   { 0: 100 },
+    count:    10,
+    children: {
         shape:      'polygon',
-        fill:       { 'cyan' : 'yellow' },
-        radius:     20,
-        rotate:      { 360: 0 },
-        duration:   1000
-        }
-    });
+    }
+});
 
-    function cueSwans() {
-        swan.style.display = 'block';
-        let t = 12;
-        let id = setInterval(frame, 750);
-        function frame() {
-            t--;
-            if (t % 2 == 0) {
-                swan.src = './media/squawk.svg';
-            } else {
-                swan.src = `./media/swan.svg`
-            }
-            if (t == 2) {
-                clearInterval(id);
-            }
+function cueSwans() {
+    swan.style.display = 'block';
+    let t = 20;
+    let id = setInterval(frame, 750);
+    function frame() {
+        t--;
+        if (t % 2 == 0) {
+            swan.src = './media/squawk.svg';
+        } else {
+            swan.src = `./media/swan.svg`
+        }
+        if (t == 2) {
+            clearInterval(id);
         }
     }
+}
 
-    function cycleMelt() {
-        let t = 12;
-        let id = setInterval(frame, 200);
-        function frame() {
-            t--;
-            menu.src = `./media/Group ${t.toString()}.png`;
-            if (t == 2) {
-                clearInterval(id);
-            }
+function cycleMelt() {
+    let t = 12;
+    let id = setInterval(frame, 200);
+    function frame() {
+        t--;
+        menu.src = `./media/Group ${t.toString()}.png`;
+        if (t == 2) {
+            clearInterval(id);
         }
     }
+}
 
 menu.addEventListener("click", (event) => {
     cycleMelt();
     setTimeout(() => {
-    event.target.style.display = "none";
-    burst.generate();
-        burst.replay();
-    text.classList.add("visible");
-    setTimeout(() => {
-        text.classList.remove("visible");
-        squawk.play();
-        cueSwans()
-    }, 2000);
+        event.target.style.display = "none";
+        burst.generate();
+            burst.replay();
+        text.classList.add("visible");
+        setTimeout(() => {
+            text.classList.remove("visible");
+            squawk.play();
+            cueSwans()
+        }, 2000);
     }, 2000);
 });
